3891e211ebc14747c3d60bdb682485df573e5300
Kam <KamronGaming@gmail.com>
2023-06-05 23:49:26 -0400

    PlayerData Cache for Skins and Player Information

--- src/main/java/noppes/mpm/client/ClientCacheHandler.java
+++ src/main/java/noppes/mpm/client/ClientCacheHandler.java
@@ -1,6 +1,7 @@
 package noppes.mpm.client;
 
 import net.minecraft.util.ResourceLocation;
+import noppes.mpm.ModelData;
 import noppes.mpm.config.ConfigClient;
 import noppes.mpm.util.CacheHashMap;
 
@@ -8,6 +9,7 @@ import java.io.File;
 
 public class ClientCacheHandler {
     private static final CacheHashMap<String, CacheHashMap.CachedObject<ImageData>> imageDataCache = new CacheHashMap<>((long) ConfigClient.CacheLife * 60 * 1000);
+    private static final CacheHashMap<String, CacheHashMap.CachedObject<ModelData>> playerData = new CacheHashMap<>((long) ConfigClient.CacheLife * 60 * 1000);
 
     public static ImageData getPlayerSkin(String directory, boolean x64, ResourceLocation resource, File file) {
         synchronized (imageDataCache) {
@@ -16,5 +18,25 @@ public class ClientCacheHandler {
             }
             return imageDataCache.get(resource.getResourcePath()).getObject();
         }
+    }
+
+    public static ModelData getPlayerData(String uuid) {
+        synchronized (playerData) {
+            if (!playerData.containsKey(uuid)) {
+                return null;
+            }
+            return playerData.get(uuid).getObject();
+        }
+    }
+
+    public static void putPlayerData(String uuid, ModelData modelData) {
+        synchronized (playerData) {
+            playerData.put(uuid, new CacheHashMap.CachedObject<>(modelData));
+        }
+    }
+
+    public static void clearCache() {
+        ClientCacheHandler.imageDataCache.clear();
+        ClientCacheHandler.playerData.clear();
     }
 }
