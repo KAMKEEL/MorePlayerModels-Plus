452b58eb5225af24046962f98fbe0df9d135e4ca
Kam <KamronGaming@gmail.com>
2024-02-18 04:28:54 -0500

    Code Cleaning

--- src/main/java/noppes/mpm/constants/EnumPacketServer.java
+++ src/main/java/noppes/mpm/constants/EnumPacketServer.java
@@ -1,23 +1,10 @@
 package noppes.mpm.constants;
 
-import kamkeel.MorePlayerModelsPermissions;
-
 public enum EnumPacketServer {
 
-	CLIENT_PING,
 	REQUEST_PLAYER_DATA,
-	PERMISSION_SEND,
 	UPDATE_PLAYER_DATA,
 	ANIMATION;
 
-	public MorePlayerModelsPermissions.Permission permission = null;
-
 	EnumPacketServer() {}
-
-	EnumPacketServer(MorePlayerModelsPermissions.Permission permission) {
-		this.permission = permission;
-	}
-	public boolean hasPermission() {
-		return permission != null;
-	}
 }
