aa131e7723ec1e29a7d4e98eb945689608626e91
Kam <KamronGaming@gmail.com>
2025-01-15 20:36:09 -0500

    Fix Arm Scaling detaching from Body

--- src/main/java/noppes/mpm/client/model/ModelScaleRenderer.java
+++ src/main/java/noppes/mpm/client/model/ModelScaleRenderer.java
@@ -1,51 +1,82 @@
 package noppes.mpm.client.model;
 
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBox;
 import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.renderer.GLAllocation;
+import net.minecraft.client.renderer.Tessellator;
 import noppes.mpm.ModelPartConfig;
 import org.lwjgl.opengl.GL11;
 
 public class ModelScaleRenderer extends ModelRenderer {
+
+    public boolean compiledModel;
+
+    /** The GL display list rendered by the Tessellator for this model */
+    public int displayListModel;
+
     protected ModelPartConfig config;
-    
+
     public float x, y, z;
 
-	public ModelScaleRenderer(ModelBase par1ModelBase) {
-		super(par1ModelBase);
-	}
+    public ModelScaleRenderer(ModelBase par1ModelBase) {
+        super(par1ModelBase);
+    }
     public ModelScaleRenderer(ModelBase par1ModelBase, int par2, int par3)
     {
         this(par1ModelBase);
-        if (((ModelMPM)par1ModelBase).x64) {
-            this.textureHeight = 64;
-        }
         this.setTextureOffset(par2, par3);
     }
-    
+
     public void setConfig(ModelPartConfig config, float x, float y, float z){
-    	this.config = config;
-    	this.x = x;
-    	this.y = y;
-    	this.z = z;
+        this.config = config;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
-	public void setRotation(ModelRenderer model, float x, float y, float z) {
-		model.rotateAngleX = x;
-		model.rotateAngleY = y;
-		model.rotateAngleZ = z;
-	}
+    public void setRotation(ModelRenderer model, float x, float y, float z) {
+        model.rotateAngleX = x;
+        model.rotateAngleY = y;
+        model.rotateAngleZ = z;
+    }
 
     public void render(float par1)
     {
-		if(!showModel || isHidden)
-			return;
+        if(!showModel || isHidden)
+            return;
+        if(!compiledModel)
+            compileDisplayListModel(par1);
         GL11.glPushMatrix();
         GL11.glTranslatef(x, y, z);
         if(config != null)
-        	GL11.glTranslatef(config.transX, config.transY, config.transZ);
+            GL11.glTranslatef(0, 0, 0);
+        this.postRender(par1);
         if(config != null)
-        	GL11.glScalef(config.scaleX, config.scaleY, config.scaleZ);
-        super.render(par1);
+            GL11.glScalef(config.scaleX, config.scaleY, config.scaleZ);
+        GL11.glCallList(this.displayListModel);
+        if (this.childModels != null)
+        {
+            for (int i = 0; i < this.childModels.size(); ++i)
+            {
+                ((ModelRenderer)this.childModels.get(i)).render(par1);
+            }
+        }
         GL11.glPopMatrix();
+    }
+
+    public void compileDisplayListModel(float par1)
+    {
+        this.displayListModel = GLAllocation.generateDisplayLists(1);
+        GL11.glNewList(this.displayListModel, GL11.GL_COMPILE);
+        Tessellator tessellator = Tessellator.instance;
+
+        for (int i = 0; i < this.cubeList.size(); ++i)
+        {
+            ((ModelBox)this.cubeList.get(i)).render(tessellator, par1);
+        }
+
+        GL11.glEndList();
+        this.compiledModel = true;
     }
 }
