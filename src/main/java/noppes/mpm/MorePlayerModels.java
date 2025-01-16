32b88c1ac2728f6ac46183bce3ca2bb7e4ebbc55
Kam <KamronGaming@gmail.com>
2025-01-15 20:36:50 -0500

    4.3

--- src/main/java/noppes/mpm/MorePlayerModels.java
+++ src/main/java/noppes/mpm/MorePlayerModels.java
@@ -32,7 +32,7 @@ public class MorePlayerModels {
 
 	@SidedProxy(clientSide = "noppes.mpm.client.ClientProxy", serverSide = "noppes.mpm.CommonProxy")
 	public static CommonProxy proxy;
-	public final static String VERSION = "4.2";
+	public final static String VERSION = "4.3";
 
 	public static FMLEventChannel Channel;
 	public static MorePlayerModels instance;
